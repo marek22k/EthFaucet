5fdfbec4cd71febf2ca4a8a846d77a64449ce92d
Marek Küthe <m.k@mk16.de>
2021-10-18 20:44:15 +0200

    Start implement web frontend

---
+++ server/web/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        
+        <title>Cheap faucet</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+        <meta name="description" content="cheap faucet for cheapeth for a small initial amount">
+        <meta name="keywords" content="faucet,cheap,cth,cheapeth">
+    </head>
+    <body>
+        <div>
+            <b>This faucet consists of two phases:</b>
+            <ul>
+                <li>In the first phase, the faucet transfers you a very small amount so that you can use the faucet (which is technically a smart contract).</li>
+                <li>In the second phase you interact with and use the faucet. This will then give you a larger amount to start with.</li>
+                </li>
+            </ul>
+        </div>
+
+    </body>
+</html>
